dd142213198d2b1b84177391cac21b5d7fb3f5a7
chunarkarvittal <vittalchunakar@gmail.com>
2026-04-10 13:47:32 +0530

    Add files via upload

---
+++ cigers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() 
+     
+    {
+int cigers,weekend;
+printf("enter the number of cigers:\n");
+scanf("%d",&cigers);
+printf("enter 0 for not wekend \n enter 1 for weekend ");
+scanf("%d",&weekend);
+if (weekend)
+{
+    if (cigers=40)
+    {
+        printf("sucess");
+    }
+    else
+    {
+    printf("fail");
+}
+}
+    else
+    {
+        if (cigers>=40 && cigers <=60)
+        {
+            printf("sucess");
+        }
+        else
+        {
+            printf("fail");
+        }
+    }
+    }
+    
